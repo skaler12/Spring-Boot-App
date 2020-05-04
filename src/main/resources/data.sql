6d28b9d703c7f4724d768ef552b175972b6714e4
skaler12 <gpoznanski98@gmail.com>
2020-05-05 00:08:16 +0200

    next commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,5 @@ INSERT INTO `role` (`description`, `name`) VALUES ('ROLE_SUPERVISOR', 'SUPERVISO
 
 INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES ('1', '1');
 
-INSERT INTO `music_store` (`equipment_type`,`store_name`,`street`, `street_number`)
-VALUES ('perkusja','perkusjak','dluga',11);
-INSERT INTO `music_store` (`equipment_type`,`store_name`,`street`, `street_number`)
-VALUES ('gitara','gitarhero','ogrodowa',132);
+
 
